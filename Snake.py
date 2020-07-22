cd9cfdd575c61d65edd2a8456d8dd1fbde7bfd47
Marco <marco.boutchelski@gmail.com>
2020-07-22 19:47:13 +0100

    Snake grows when eating food

--- Snake.py
+++ Snake.py
@@ -1,4 +1,5 @@
 import math
+import random
 import pygame
 import sys
 
@@ -9,23 +10,30 @@ class Square:
     squareSize = 40
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.rect = pygame.Rect(self.x, self.y, squareSize, squareSize)
+        self.rect = pygame.Rect(x, y, squareSize, squareSize)
 
-    def draw(self):
-        pygame.draw.rect(screen, (255, 0, 0), self.rect, 0)
+    def draw(self, color):
+        pygame.draw.rect(screen, color, self.rect, 0)
 
-    def move(self, vector):
-        self.rect.move_ip(vector.x, vector.y)
-        if self.rect.y > screenHeight-squareSize:
+    def moveHead(self, vector):
+        self.rect.move_ip(round(vector.x), round(vector.y))
+        if self.rect.y > screenHeight - squareSize:
             self.rect.y = 0
-        if self.rect.x > screenWidth-squareSize:
+        if self.rect.x > screenWidth - squareSize:
             self.rect.x = 0
         if self.rect.y < 0:
             self.rect.y = screenHeight
         if self.rect.x < 0:
             self.rect.x = screenWidth
+
+    def getCoords(self):
+        coords = pygame.Vector2()
+        coords[:] = self.rect.x, self.rect.y
+        return coords
+
+    def setCoords(self, coords):
+        self.rect.x = round(coords.x)
+        self.rect.y = round(coords.y)
 
 
 class Snake:
@@ -36,7 +44,10 @@ class Snake:
 
     def draw(self):
         for i in self.body:
-            Square.draw(i)
+            Square.draw(i, (255, 0, 0))
+
+    def getBody(self):
+        return self.body
 
     def move(self):
 
@@ -47,37 +58,63 @@ class Snake:
 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_w or event.type == pygame.K_UP:
-                    if move != (0, squareSize):
-                        move[:] = 0, -squareSize
+                    if moveCoords != (0, squareSize):
+                        moveCoords[:] = 0, -squareSize
                 elif event.key == pygame.K_s or event.type == pygame.K_DOWN:
-                    if move != (0, -squareSize):
-                        move[:] = 0, squareSize
+                    if moveCoords != (0, -squareSize):
+                        moveCoords[:] = 0, squareSize
                 elif event.key == pygame.K_d or event.type == pygame.K_RIGHT:
-                    if move != (-squareSize, 0):
-                        move[:] = squareSize, 0
+                    if moveCoords != (-squareSize, 0):
+                        moveCoords[:] = squareSize, 0
                 elif event.key == pygame.K_a or event.type == pygame.K_LEFT:
-                    if move != (squareSize, 0):
-                        move[:] = -squareSize, 0
+                    if moveCoords != (squareSize, 0):
+                        moveCoords[:] = -squareSize, 0
 
-        self.head.move(move)
+
+
+        if self.head.getCoords() == food.Square.getCoords():  # Food eaten
+            food.respawnFood()
+            self.body.append(Square(self.head.rect.x, self.head.rect.y))
+
+        for i in range(len(self.body) - 1, 0, -1):
+            self.body[i].rect.x = self.body[i-1].rect.x
+            self.body[i].rect.y = self.body[i-1].rect.y
+
+        self.head.moveHead(moveCoords)
+
+
+class Food:
+    def __init__(self, x, y):
+        self.Square = Square(x, y)
+
+    def drawFood(self):
+        self.Square.draw((0, 255, 0))
+
+    def respawnFood(self):
+        coords = pygame.Vector2()
+        coords[:] = random.randint(0, (screenWidth - squareSize) / squareSize) * squareSize, random.randint(0, (screenHeight - squareSize) / squareSize) * squareSize
+        self.Square.setCoords(coords)
 
 
 def drawScreen():
     screen.fill((0, 0, 0))
-    Snake.draw(snake)
+    snake.draw()
+    food.drawFood()
     pygame.display.update()
 
 
 def main():
-    global screen, screenWidth, screenHeight, snake, move
+    global screen, screenWidth, screenHeight, snake, moveCoords, food
 
-    move = pygame.Vector2()
+    moveCoords = pygame.Vector2()
 
     screenWidth = 1280
     screenHeight = 720
 
     screen = pygame.display.set_mode((screenWidth, screenHeight))
     snake = Snake(math.floor(screenWidth / 2 - squareSize), math.floor(screenHeight / 2 - squareSize))
+    food = Food(random.randint(0, (screenWidth - squareSize) / squareSize) * squareSize,
+                random.randint(0, (screenHeight - squareSize) / squareSize) * squareSize)
 
     clock = pygame.time.Clock()
 
